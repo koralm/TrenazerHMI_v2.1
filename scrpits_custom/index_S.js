4fa385b3335d956278f64bef8051f50fa78492fb
karol.miadlicki@zut.edu.pl <CFQuwpO9asYIqQGMuZlg>
2018-07-09 20:28:50 +0200

    Zmiany klawiatura:             'normal' : [                 '0 1 2',                 '3 4 5',                 '6 7 8',                 '{a} {bksp} ,'             ]

--- scrpits_custom/index_S.js
+++ scrpits_custom/index_S.js
@@ -32,9 +32,10 @@ $(function () {
         layout: 'custom',
         customLayout: {
             'normal' : [
-                '0 1 2 3',
-                '4 5 6 7',
-                '8 9 {bksp} {a}'
+                '0 1 2',
+                '3 4 5',
+                '6 7 8',
+                '{a} {bksp} ,'
             ]
         },
         usePreview: false,
--- scrpits_custom/settings_advenced_S.js
+++ scrpits_custom/settings_advenced_S.js
@@ -56,9 +56,10 @@ $(function () {
         layout: 'custom',
         customLayout: {
             'normal' : [
-                '0 1 2 3',
-                '4 5 6 7',
-                '8 9 {bksp} {a}'
+                '0 1 2',
+                '3 4 5',
+                '6 7 8',
+                '{a} {bksp} ,'
             ]
         },
         autoAccept : true,
@@ -72,9 +73,10 @@ $(function () {
         layout: 'custom',
         customLayout: {
             'normal' : [
-                '0 1 2 3',
-                '4 5 6 7',
-                '8 9 {bksp} {a}'
+                '0 1 2',
+                '3 4 5',
+                '6 7 8',
+                '{a} {bksp} ,'
             ]
         }
         ,
